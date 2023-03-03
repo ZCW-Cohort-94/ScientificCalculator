936ad97b9f88e7c1da876dc623947801daafe5f9
adams <stilwelladam8@gmail.com>
2023-03-03 18:07:43 -0500

    Added more classes and stuff.

--- src/main/java/com/zipcodewilmington/scientificcalculator/CoreApplications.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/CoreApplications.java
@@ -1,5 +1,19 @@
 package com.zipcodewilmington.scientificcalculator;
 
 public class CoreApplications {
+//    Core Features
+//    All calculators should have the following features:
+//
+//    A state, representing the value currently displayed on the calculator (default 0) *
+//    Get the current number on the display *
+//    Clear the display *
+//    Change the number on the display *
+//    Add, subtract, multiply, and divide the value on the display by a given number
+//    Calculate the square (x2) and square root (√x) of the number on the display *
+//    Calculate variable exponentiation (xy)
+//    Calculate the inverse of the number on the display (1/x) *
+//    Invert the sign of the number on the display (switch between positive and negative)
+//    Update the display to Err if an error occurs (eg: Division by zero) *
+//    Errors must be cleared before any other operation can take place *
 
 }
