aeb743b6edd4a2fc2b600e9ebd1bc03ae48708b2
joe-trung <kieutrungtran@gmail.com>
2023-03-04 13:42:01 -0500

    Good morning Saturday at 13

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScientificApplications.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificApplications.java
@@ -12,14 +12,8 @@ public class ScientificApplications {
         Scanner choice = new Scanner(System.in);
         int unitChoice = choice.nextInt();
 
-        double y = getInputX();
-
-        if (unitChoice!=1||unitChoice!=2) {
+        if ((unitChoice!=1) || (unitChoice!=2)) {
             System.out.println("Invalid Choice");
-            Break;
-        }
-        if (unitChoice==2) {
-            y = y*Math.PI/180;
         }
 
         System.out.println("Please select one of the following trigonometry functions:");
@@ -35,28 +29,24 @@ public class ScientificApplications {
         int entryMode = choice.nextInt();
         if (entryMode < 1 || entryMode > 6) {
             System.out.println("Invalid Choice");
-            break;
         } else {
+            double x = getDoubleInputX();
+            if (unitChoice==2) {
+                x = x*Math.PI/180;
+            }
             switch (entryMode) {
                 case 1:
-                    System.out.println("Sine Function");
-                    Sine(y);
-
+                    System.out.println("Sine results of "+x+" is:"+Math.sin(x));
                 case 2:
-                    System.out.println("Cosine Function");
-                    Cosine(y);
+                    System.out.println("Cosine results of "+x+" is:"+Math.cos(x));
                 case 3:
-                    System.out.println("Tangent Function");
-                    Tangent(y);
+                    System.out.println("Tangent results of "+x+" is:"+Math.tan(x));
                 case 4:
-                    System.out.println("Inverse Sine Function");
-                    InverseSine(y);
+                    System.out.println("Inverse Sine results of "+x+" is:"+Math.asin(x));
                 case 5:
-                    System.out.println("Inverse Cosine Function");
-                    InverseCosine(y);
+                    System.out.println("Inverse Cosine results of "+x+" is:"+Math.asin(x));
                 case 6:
-                    System.out.println("Inverse Tangent Function");
-                    InverseTangent(y);
+                    System.out.println("Inverse Tan results of "+x+" is:"+Math.atan(x));
             }
         }
     }
@@ -64,7 +54,6 @@ public class ScientificApplications {
 
 //menu scientific logarithmic and factorial functions
     public static void logarithmicMenu() {
-        double y = getInputX();
 
         System.out.println("Please select one of the following logarithmic or factorial functions:");
         System.out.println("Enter \"1\" for Log Function");
@@ -73,177 +62,74 @@ public class ScientificApplications {
         System.out.println("Enter \"5\" for Inverse Ln Function");
 
         System.out.println();
-
+        Scanner choice = new Scanner(System.in);
         int entryMode = choice.nextInt();
-        if (entryMode < 1 || entryMode > 6) {
+        if (entryMode < 1 || entryMode > 5) {
             System.out.println("Invalid Choice");
-            break;
         } else {
             switch (entryMode) {
                 case 1:
-                    System.out.println("Sine Function");
-                    Sine(y);
+                    int y = getInInput();
+                    System.out.println("Logarithm results of "+y+" is:"+ Math.log10(y));
 
                 case 2:
-                    System.out.println("Cosine Function");
-                    Cosine(y);
+                    y = getInInput();
+                    System.out.println("Inverse Log results of "+y+" is:"+ Math.pow(10,y));
+
                 case 3:
-                    System.out.println("Tangent Function");
-                    Tangent(y);
+                    y = getInInput();
+                    System.out.println("Natural Log results of "+y+" is:"+ Math.log(y));
+
                 case 4:
-                    System.out.println("Inverse Sine Function");
-                    InverseSine(y);
+                    y = getInInput();
+                    System.out.println("Inverse Natural Log results of "+y+" is:"+ Math.exp(y));
                 case 5:
-                    System.out.println("Inverse Cosine Function");
-                    InverseCosine(y);
-                case 6:
-                    System.out.println("Inverse Tangent Function");
-                    InverseTangent(y);
+                    y = getInInput();
+                    FactorialFunction(y);
             }
         }
     }
-// Ask for input number
-    private static double getInputX() {
+
+// Ask for input  double number
+    private static double getDoubleInputX() {
             Scanner inScan = new Scanner(System.in);
             System.out.println("Please enter x value for calculation:");
-            double x = inScan.nextDouble();
-            inScan.close();
-            return x;
+            return inScan.nextDouble();
         }
+// Ask for input int number
+    private static int getInInput() {
+        Scanner inScan = new Scanner(System.in);
+        System.out.println("Please enter x value for calculation:");
+        return inScan.nextInt();
     }
-
-//Sine function
-    private static double Sine(double x) {
-        System.out.println("Sine results of "+x+" is:");
-        return Math.sin(x);
-    }
-
-//Cosine function
-    public static double Cosine(double x) {
-        System.out.println("Cosine results of "+x+" is:");
-        return Math.cos(x);
-    }
-
-//Tangent Function
-    public static double Tangent(double x) {
-        System.out.println("Tangent results of "+x+" is:");
-        return Math.tan(x);
-    }
-
-    //Inversine Function
-    public static double InverseSine(double x) {
-        System.out.println("Inverse Sine results of "+x+" is:");
-        return Math.asin(x);
-    }
-
-    //Inverse Cosine Function
-    public static double InverseCosine(double x) {
-        System.out.println("Inverse Cosine results of "+x+" is:");
-        return Math.acos(x);
-    }
-
-    //Inverse Tangent Function
-    public static double InverseTangent(double x) {
-        System.out.println("Inverse Tangent results of "+x+" is:");
-        return Math.atan(x);
-    }
-
-
-
-
-// menu binary , octal, decimal, hexadecimal
-public static void modeMenu(); {
-        Scanner choice = new Scanner(System.in);
-        System.out.println("Please select one of the following functions:");
-        System.out.println("Enter \"1\" for Binary Mode");
-        System.out.println("Enter \"2\" for Octal Mode");
-        System.out.println("Enter \"3\" for Decimal Mode");
-        System.out.println("Enter \"4\" for Hexadecimal Mode");
-        System.out.println();
-        int entryMode = choice.nextInt();
-        if (entryMode < 1 || entryMode > 4) {
-        System.out.println("Invalid Choice");
-        break;
-        } else {
-        switch (entryMode) {
-        case 1:
-        System.out.println("Welcome to Binary Mode");
-        case 2:
-        System.out.println("Welcome to Octal Mode");
-        case 3:
-        System.out.println("Welcome to Decimal Mode");
-        case 4:
-        System.out.println("Welcome to Hexadecimal Mode");
-        }
-        }
-        }
-
-    public static void switchDisplayMode() {
-
-
-
-
-
-    } //switch display mode
-
-    public class String switchDisplayMode() {
-
-
-    } //switch display mode string
-
-
-
-
-/*
-Memory - Store up to one numeric value in memory for recall later (default to 0) *
-        (M+ key) Add the currently displayed value to the value in memory (store in memory and update display) *
-        (MC key) Reset memory *
-        (MRC key) Recall the current value from memory to the display *
-*/
-    public class Memory() { }
-
-
-
-
-
-
-
-
-
-
-
-    public class switchUnitsMode() {}
-    public class String switchUnitsMode() {}
-
-//Logarithm
-    public static double calLog(int x) {
-        System.out.println("Logarithm results of "+x+" is:");
-        return Math.log(x);
-}
-
-// Inverse Logarithm
-    public static double InverseLogarithm(int x) {
-        System.out.println("Inverse Logarithm results of "+x+" is:");
-        return Math.exp(x);
-    }
-
-//Natural Logarithm
-    public static double NaturalLogarithm(double x) {
-        System.out.println("Natural Logarithm results of "+x+" is:");
-        return Math.log(x);
-    }
-
 
 //Factorial calculation
     public static int FactorialFunction(int n) {
         int factorialResult = 1;
-        for (int i = n; i>0; i--) {
-            factorialResult = factorialResult*i;
+        for (int i = n; i > 0; i--) {
+            factorialResult = factorialResult * i;
         }
-        System.out.println("Factorial results of "+n+" is:");
+        System.out.println("Factorial results of " + n + " is:"+factorialResult);
         return factorialResult;
-        }
     }
+
+
+/*
+Memory - Store up to one numeric value in memory for recall later (default to 0) *
+    (M+ key) Add the currently displayed value to the value in memory (store in memory and update display) *
+    (MC key) Reset memory *
+    (MRC key) Recall the current value from memory to the display *
+*/
+public class Memory{}
+
+//// menu binary , octal, decimal, hexadecimal
+//public static modeMenu() {
+//
+//        } //switch display mode
+//
+//    public class String switchDisplayMode() {}
+
+
 
 
 
