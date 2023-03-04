8160ca5b2b3043645070e52673b48925af692d20
Nora <sxj042@gmail.com>
2023-03-04 11:14:24 -0500

    Added some methods

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -12,6 +12,46 @@ public class MainApplication {
 
         Console.println("The user input %s as a string", s);
         Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+        Console.println("The user input %s as a double", d);
     }
-}
+
+    public int addition(int x, int y) {
+        return x + y;
+    }
+
+    public int subtraction(int x, int y) {
+        return x - y;
+    }
+
+    public int multiplication(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+    public int square(int x) {
+        return x * x;
+    }
+
+    public double squareRoot(double x) {
+        return Math.sqrt(x);
+    }
+
+    public double inverse(double x) {
+        return (1 / x);
+    }
+
+    public double exponent(double x, double y) {
+        return Math.pow(x, y);
+    }
+
+    public double switchSign(double x){
+        return(x *-1.0);
+    }
+
+
+} // class
+
+
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,16 @@
 package com.zipcodewilmington.scientific_calculator;
 
+import com.zipcodewilmington.scientificcalculator.MainApplication;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 2/9/18.
  */
 public class TestMainApplication {
+    @Test
+    public void testAdd(){
+        MainApplication test = new MainApplication();
+        Assert.assertEquals(4, test.addition(1,3));
+    }
 }
