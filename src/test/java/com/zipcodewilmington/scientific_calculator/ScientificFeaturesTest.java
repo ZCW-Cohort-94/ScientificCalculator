5613f8e30a6143bb8dcabc30f47c7d133b9ccf2f
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-04 18:33:53 -0500

    Added SciFeat tests

---
+++ src/test/java/com/zipcodewilmington/scientific_calculator/ScientificFeaturesTest.java
@@ -0,0 +1,50 @@
+package com.zipcodewilmington.scientific_calculator;
+mport com.zipcodewilmington.scientificcalculator.ScientificFeatures;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+public class ScientificFeaturesTest {
+    public ScientificFeatures scientificFeatures;
+    @Before
+    public void setup(){scientificFeatures = new ScientificFeatures();}
+    @Test
+    public void sinTest() {
+        double expected = 0.77;
+        double actual = scientificFeatures.getSin(70);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+
+    @Test
+    public void cosinTest() {
+        double expected = 0.862;
+        double actual = scientificFeatures.getCos(100);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+    @Test
+    public void tanTest() {
+        double expected = 11.87;
+        double actual = scientificFeatures.getTan(856);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+    @Test
+    public void invSinTest() {
+        double expected = -0.53;
+        double actual = scientificFeatures.getInvSin(34);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+
+    @Test
+    public void invCosinTest() {
+        double expected = 0.633;
+        double actual = scientificFeatures.getInvCos(100);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+
+    @Test
+    public void invTanTest() {
+        double expected = 0.633;
+        double actual = scientificFeatures.getInvTan(100);
+        Assert.assertEquals(expected, actual, 0.02);
+    }
+
+}
