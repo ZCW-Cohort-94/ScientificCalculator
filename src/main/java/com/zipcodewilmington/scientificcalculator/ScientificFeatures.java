0e3e18d6998f36eceb9cabe61833787ff6f7f694
Adam-Barto <adam.jbarto@gmail.com>
2023-03-05 14:23:49 -0500

    Pythagorean Theorem that returns C

--- src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFeatures.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/ScientificFeatures.java
@@ -176,7 +176,7 @@ public class ScientificFeatures {
         }
         return result;
     }
-
-
-
+    public static double getPythagoreanTheorem(double a, double b){
+        return (Math.sqrt(Math.pow(a,2) + Math.pow(b,2)));
+    }
 }
