e249c16fc92c4c48670a19bbc0cbbb3c594709c2
Nora <sxj042@gmail.com>
2023-03-05 10:41:59 -0500

    updated files worked on by Zach and Nora

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -3,7 +3,6 @@ package com.zipcodewilmington.scientific_calculator;
 import com.zipcodewilmington.scientificcalculator.MainApplication;
 import org.junit.Assert;
 import org.junit.Test;
-import org.sonatype.guice.bean.containers.Main;
 
 /**
  * Created by leon on 2/9/18.
@@ -30,105 +29,125 @@ public class TestMainApplication {
         testCalc.multiplication(3.0);
         Assert.assertEquals(0, testCalc.memoryValue, 0.0);
     }
-//    @Test
-//    public void testDivision(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(2, test.division(6,3));
-//    }
-//    @Test
-//    public void testSquare(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(9, test.square(3));
-//    }
-//    @Test
-//    public void testSqRt(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(3.0, test.squareRoot(9.0), 0);
-//        // delta: the maximum delta between expected and actual for which both numbers are still considered equal
-//    }
-//    @Test
-//    public void testInverse(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(0.2, test.inverse(5), 0);
-//    }
-//    @Test
-//    public void testExponent(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(81, test.exponent(3, 4), 0);
-//    }
-//    @Test
-//    public void testSwitchSign(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(5.5, test.switchSign(-5.5), 0);
-//    }
-//
-//    @Test
-//    public void testSin(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(0.199, test.sine(0.2), 0.001);
-//    }
-//    @Test
-//    public void testCos(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(1, test.cosine(0), 0.001);
-//    }
-//    @Test
-//    public void testTan(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(0.309, test.tangent(0.3), 0.001);
-//    }
-//    @Test
-//    public void testInverseSin(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(1.571, test.inverseSine(1), 0.001);
-//    }
-//    @Test
-//    public void testInverseCos(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(1.266, test.inverseCosine(0.3), 0.001);
-//    }
-//    @Test
-//    public void testInverseTan(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(0.464, test.inverseTangent(0.5), 0.001);
-//    }
-//    @Test
-//    public void testLog(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(0.699, test.log(5), 0.001);
-//    }
-//    @Test
-//    public void testInverseLog(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(100, test.inverseLog(2), 0.001);
-//    }
-//    @Test
-//    public void testNaturalLog(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(1.609, test.naturalLog(5), 0.001);
-//    }
-//    @Test
-//    public void testInverseNatLog(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(403.4288, test.inverseNatLog(6), 0.001);
-//    }
-//
-//    @Test
-//    public void testFactorial(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(120, test.factorial(5));
-//    }
-//
-//    @Test
-//    public void testCubedRt(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals(4, test.cubed(64), 0.001);
-//    }
+    @Test
+    public void testDivision(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.division(2);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.0);
+    }
+    @Test
+    public void testSquare(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.square(2); // Make sure works
+        Assert.assertEquals(0, testCalc.memoryValue,0.0);
+    }
+    @Test
+    public void testSqRt(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.squareRoot(2); // Make sure works
+        Assert.assertEquals(0, testCalc.memoryValue, 0.0);
+        // delta: the maximum delta between expected and actual for which both numbers are still considered equal
+    }
+    @Test
+    public void testInverse(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.inverse(0); //make sure works
+        Assert.assertEquals(0, testCalc.memoryValue,0.0);
+    }
+    @Test
+    public void testExponent(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.exponent(2,3); //make sure Expected is 1 value || is it 2
+        Assert.assertEquals(0, testCalc.memoryValue, 0);
+    }
+    @Test
+    public void testSwitchSign(){
+        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+        testCalc.switchSign(2);
+        Assert.assertEquals(-0, testCalc.memoryValue, 0.0);
+    }
+
+    @Test
+    public void testSin(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.sine(0); //make sure works
+        Assert.assertEquals(0, testCalc.memoryValue, 0);
+    }
+    @Test
+    public void testCos(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.cosine(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
+    @Test
+    public void testTan(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.tangent(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
+    @Test
+    public void testInverseSin(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.inverseSine(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
+    @Test
+    public void testInverseCos(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.inverseCosine(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.001);
+    }
+    @Test
+    public void testInverseTan(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.inverseTangent(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.001);
+    }
+    @Test
+    public void testLog(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.log(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.001);
+    }
+    @Test
+    public void testInverseLog(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.inverseLog(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
+    @Test
+    public void testNaturalLog(){
+        MainApplication.SciCalc testCalc= new MainApplication.SciCalc();
+        testCalc.naturalLog(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.001);
+    }
+    @Test
+    public void testInverseNatLog(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.inverseNatLog(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
+
+    @Test
+    public void testFactorial(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.factorial(0);
+        Assert.assertEquals(0, testCalc.memoryValue,0);
+    }
+
+    @Test
+    public void testCubedRt(){
+        MainApplication.SciCalc testCalc = new MainApplication.SciCalc();
+        testCalc.cubed(0);
+        Assert.assertEquals(0, testCalc.memoryValue, 0.000);
+    }
 //    @Test
 //    public void testHello(){
-//        MainApplication test = new MainApplication();
-//        Assert.assertEquals("01134", test.hello());
+//        MainApplication.Calculator testCalc = new MainApplication.Calculator();
+//        testCalc.hello(01134);
+//        Assert.assertEquals("Hello", testCalc.memoryValue, 0.00);
 //    }
-//
+
 
 } // class
+
